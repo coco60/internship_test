e5e019f86d71f7ee1d94d00266b027bd60485a91
Coralie <coralie.verrier@etu.utc.fr>
2018-04-16 22:29:04 +0200

    clean of pom.xml + last method POST

--- src/main/scala/myproject/spring/schema/AppController.scala
+++ src/main/scala/myproject/spring/schema/AppController.scala
@@ -1,21 +1,27 @@
 package myproject.spring.schema
 
 import java.io._
+import java.net.URLDecoder._
+import scala.io.Source
+import util.control.Breaks._
 import collection.JavaConversions._
+import org.json4s.jackson.JsonMethods._
+import org.json4s._
+import org.json4s.JsonDSL._
+
 import com.github.fge.jsonschema.main.JsonSchema
 import com.github.fge.jsonschema.main.JsonSchemaFactory
 import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.core.report.ProcessingMessage
+
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation._
-import java.net.URLDecoder._
-import org.json4s.jackson.JsonMethods._
-import org.json4s._
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.json4s.JsonDSL._
-import scala.io.Source
-import util.control.Breaks._
-import scala.collection.mutable.Map
+
 
 @RestController
 class AppController {
@@ -36,7 +42,7 @@ class AppController {
 
     // verify JSON schema format
       if(asJsonNode(source) != null){
-
+    // the file with all the schemas will be stored in the user home directory
         val path = System.getProperty("user.home")
 
     // verify that schema with id given isn't in schemaLibrary.txt
@@ -44,7 +50,6 @@ class AppController {
         if(new java.io.File(path+"/schemaLibrary.txt").exists)
           breakable {
             val lines = Source.fromFile(path+"/schemaLibrary.txt").getLines
-
             lines.foreach(line =>
               if(id == line){
                 idAlreadyUsed = true
@@ -87,11 +92,12 @@ class AppController {
 
       // Get schema with id
       breakable {
-        lines.foreach(line =>
+        lines.foreach{line =>
           if(id == line){
             currentSchema = lines.next()
             break
-          })
+          }
+        }
       }
 
       if(currentSchema == null)
@@ -110,31 +116,52 @@ class AppController {
   }
 
   // validate json
-  // @RequestMapping(value = Array("/validate/{id}"), method = Array(RequestMethod.POST))
-  // @ResponseStatus(HttpStatus.OK)
-  // def validate(@PathVariable id: String, @RequestBody fileToValidate: String) : String = {
-  //   val jsonToValidate = asJsonNode(compact(render(parse(decode(fileToValidate, "UTF-8")))))
-  //
-  //   try {
-  //       serverAnswer = get("http://localhost:8080/schema/"+id)
-  //
-  //       val schema : JsonSchema  = asJsonNode(currentSchema)
-  //       val validator = factory.getValidator
-  //       val processingReport = validator.validate(currentSchema, jsonToValidate)
-  //
-  //       if (processingReport.isSuccess) {
-  //         serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
-  //         ~ ("status" -> "SUCCESS")))
-  //       } else
-  //           serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
-  //           ~ ("status" -> "ERROR")))
-  //
-  //   } catch{
-  //     case e: Exception =>
-  //       serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
-  //       ~ ("status" -> "ERROR")))
-  //   }
-  //   serverAnswer
-  // }
+  @RequestMapping(value = Array("/validate/{id}"), method = Array(RequestMethod.POST))
+  @ResponseStatus(HttpStatus.OK)
+  def validate(@PathVariable id: String, @RequestBody fileToValidate: String) : String = {
+
+    mapper.registerModule(DefaultScalaModule)
+    mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false)
+    mapper.registerModule(DefaultScalaModule)
+
+
+    try {
+
+        serverAnswer = download(id)
+        var jsonToValidate = parse(decode(fileToValidate, "UTF-8"))
+
+      // take off fields with null values
+        var result = mapper.writeValueAsString(jsonToValidate.values)
+
+        val jsonToValidateChecked : JsonNode = mapper.readTree(result)
+        val nodeSchema : JsonNode = mapper.readTree(currentSchema)
+        val schema : JsonSchema = factory.getJsonSchema(nodeSchema)
+        val processingReport : ProcessingReport = schema.validate(jsonToValidateChecked)
+
+        if (processingReport.isSuccess) {
+          serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
+          ~ ("status" -> "SUCCESS")))
+        } else{
+
+          // get all errors from ProcessingReport value
+          val report = processingReport.iterator()
+          var message = "Message : "
+
+          while(report.hasNext){
+            var reportMessage : ProcessingMessage = report.next()
+            message = message + reportMessage.asJson().get("message").asText() + ". ";
+          }
+
+          serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
+          ~ ("status" -> "ERROR") ~ ("message" -> message)))
+        }
+
+    } catch{
+      case e: Exception =>
+        serverAnswer = compact(render(("action" -> "validateDocument") ~ ("id" -> id)
+        ~ ("status" -> "ERROR")))
+    }
+    serverAnswer
+  }
 
 }
